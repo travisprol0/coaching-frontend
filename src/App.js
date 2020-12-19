d6fd04fb5784a3b195d68d50d77e2702dba22ca7
travisprol0 <travisjohnprol@gmail.com>
2020-12-19 10:28:09 -0500

    email js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
-import "./css/App.css";
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./css/App.css"
+import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import NavBar from "./components/NavBar"
-import Home from "./components/Home";
+import Home from "./components/Home"
 import Info from "./components/Info"
 import Contact from "./components/Contact"
 import Recipes from "./components/Recipes"
 
 class App extends React.Component {
-
-  state = {
-    path: "/"
-  }
-
-  pathfinder = (path) => {
-    return path
-  }
-
- 
   render() {
     return (
       <div>
         <Router>
-          <Route  path="/" component={NavBar} />
+          <Route path="/" component={NavBar} />
           <Route exact path="/" component={Home} />
           <Route exact path="/info" component={Info} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/recipes" component={Recipes} />
         </Router>
-        
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/forms/InfoForm.js
+++ src/forms/InfoForm.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from "react"
+import emailjs from "emailjs-com"
 
 class InfoForm extends React.Component {
   state = {
@@ -8,84 +9,196 @@ class InfoForm extends React.Component {
     iphone: "",
     email: "",
     message: "",
-  };
+    success: "pre email",
+  }
 
   changeHandler = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  renderForm = () => {
+    if (this.state.success === "pre email") {
+      return (
+        <form onSubmit={this.sendEmail}>
+          <span>
+            First Name:
+            <input
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              placeholder="First Name"
+              onChange={this.changeHandler}
+            ></input>
+          </span>
+          <br></br>
+          <span>
+            Last Name:
+            <input
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              placeholder="Last Name"
+              onChange={this.changeHandler}
+            ></input>
+          </span>
+          <br></br>
+          <span>
+            Phone Number:
+            <input
+              type="number"
+              name="phoneNumber"
+              value={this.state.phoneNumber}
+              placeholder="Phone Number"
+              onChange={this.changeHandler}
+            ></input>
+          </span>
+          <br></br>
+          {/* <span>
+        iPhone?
+        <select
+          name="iphone"
+          value={this.state.iphone}
+          onChange={this.changeHandler}
+        >
+          <option>Choose One</option>
+          <option>Yes</option>
+          <option>No</option>
+        </select>
+      </span>
+      <br></br> */}
+          <span>
+            Email:
+            <input
+              type="text"
+              name="email"
+              value={this.state.email}
+              placeholder="Email"
+              onChange={this.changeHandler}
+            ></input>
+          </span>
+          <br></br>
+          <span>
+            Message:
+            <input
+              type="text"
+              name="message"
+              value={this.state.message}
+              placeholder="Message"
+              onChange={this.changeHandler}
+            ></input>
+          </span>
+          <br></br>
+          <input type="submit" value="Send Message"></input>
+        </form>
+      )
+    } else if (this.state.success === "false") {
+      return (
+        <>
+          <h1>Something went wrong! Please try again!</h1>
+          <form onSubmit={this.sendEmail}>
+            <span>
+              First Name:
+              <input
+                type="text"
+                name="firstName"
+                value={this.state.firstName}
+                placeholder="First Name"
+                onChange={this.changeHandler}
+              ></input>
+            </span>
+            <br></br>
+            <span>
+              Last Name:
+              <input
+                type="text"
+                name="lastName"
+                value={this.state.lastName}
+                placeholder="Last Name"
+                onChange={this.changeHandler}
+              ></input>
+            </span>
+            <br></br>
+            <span>
+              Phone Number:
+              <input
+                type="number"
+                name="phoneNumber"
+                value={this.state.phoneNumber}
+                placeholder="Phone Number"
+                onChange={this.changeHandler}
+              ></input>
+            </span>
+            <br></br>
+            {/* <span>
+        iPhone?
+        <select
+          name="iphone"
+          value={this.state.iphone}
+          onChange={this.changeHandler}
+        >
+          <option>Choose One</option>
+          <option>Yes</option>
+          <option>No</option>
+        </select>
+      </span>
+      <br></br> */}
+            <span>
+              Email:
+              <input
+                type="text"
+                name="email"
+                value={this.state.email}
+                placeholder="Email"
+                onChange={this.changeHandler}
+              ></input>
+            </span>
+            <br></br>
+            <span>
+              Message:
+              <input
+                type="text"
+                name="message"
+                value={this.state.message}
+                placeholder="Message"
+                onChange={this.changeHandler}
+              ></input>
+            </span>
+            <br></br>
+            <input type="submit" value="Send Message"></input>
+          </form>
+        </>
+      )
+    } else if (this.state.success === "true") {
+      return <h1>Thank you! You'll hear back from me soon!</h1>
+    }
+  }
+
+  sendEmail = (e) => {
+    e.preventDefault()
+    // service_4fh66ju
+    emailjs
+      .sendForm(
+        "service_4fh66ju",
+        "template_oik5t0k",
+        e.target,
+        "user_qGTcZI0dV3MS1J6d7CirL"
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          this.setState({ success: "true" })
+        },
+        (error) => {
+          console.log(error.text)
+          this.setState({ success: "false" })
+        }
+      )
+  }
+
   render() {
-    return (
-      <form>
-        <span>
-          First Name:
-          <input
-            type="text"
-            name="firstName"
-            value={this.state.firstName}
-            placeholder="First Name"
-            onChange={this.changeHandler}
-          ></input>
-        </span>
-        <br></br>
-        <span>
-          Last Name:
-          <input
-            type="text"
-            name="lastName"
-            value={this.state.lastName}
-            placeholder="Last Name"
-            onChange={this.changeHandler}
-          ></input>
-        </span>
-        <br></br>
-        <span>
-          Phone Number:
-          <input
-            type="number"
-            name="phoneNumber"
-            value={this.state.phoneNumber}
-            placeholder="Phone Number"
-            onChange={this.changeHandler}
-          ></input>
-        </span>
-        <br></br>
-        <span>
-          iPhone?
-          <select
-            name="iphone"
-            value={this.state.iphone}
-            onChange={this.changeHandler}
-          >
-            <option>Choose One</option>
-            <option>Yes</option>
-            <option>No</option>
-          </select>
-        </span>
-        <br></br>
-        <span>
-          Email:
-          <input
-            type="text"
-            name="email"
-            value={this.state.email}
-            placeholder="Email"
-            onChange={this.changeHandler}
-          ></input>
-        </span>
-        <br></br>
-        <span>
-          Message:
-          <textarea
-            type="text"
-            name="message"
-            value={this.state.message}
-            placeholder="Message"
-            onChange={this.changeHandler}
-          ></textarea>
-        </span>
-      </form>
-    );
+    console.log(this.state.success)
+    return <>{this.renderForm()}</>
   }
 }
 
-export default InfoForm;
+export default InfoForm
