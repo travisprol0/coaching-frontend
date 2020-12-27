cf257d668657bbd2409b3d05cc60e211569e4280
travisprol0 <travisjohnprol@gmail.com>
2020-12-27 11:37:23 -0500

    made app component function based

--- src/App.js
+++ src/App.js
@@ -12,25 +12,23 @@ import Motivation from "./components/coaching/Motivation"
 import Contact from "./components/coaching/Contact"
 import ActingHome from "./components/acting/ActingHome"
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Router>
-          <Route exact path="/" component={Home} />
-          <Route path="/coach" component={CoachNavigation} />
-          <Route exact path="/coach" component={CoachingHome} />
-          <Route exact path="/coach/about-me" component={AboutMe} />
-          <Route exact path="/coach/fitness" component={Fitness} />
-          <Route exact path="/coach/nutrition" component={Nutrition} />
-          <Route exact path="/coach/wellness" component={Wellness} />
-          <Route exact path="/coach/motivation" component={Motivation} />
-          <Route exact path="/coach/contact-me" component={Contact} />
-          <Route exact path="/actor" component={ActingHome} />
-        </Router>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/coach" component={CoachNavigation} />
+        <Route exact path="/coach" component={CoachingHome} />
+        <Route exact path="/coach/about-me" component={AboutMe} />
+        <Route exact path="/coach/fitness" component={Fitness} />
+        <Route exact path="/coach/nutrition" component={Nutrition} />
+        <Route exact path="/coach/wellness" component={Wellness} />
+        <Route exact path="/coach/motivation" component={Motivation} />
+        <Route exact path="/coach/contact-me" component={Contact} />
+        <Route exact path="/actor" component={ActingHome} />
+      </Router>
+    </div>
+  )
 }
 
 export default App
